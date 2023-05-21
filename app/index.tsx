8625d646c1c3f2cf9dbcdec26c252971a242a2fc
Cast01 <gabriel_floriano_castilho@outlook.com>
2023-05-21 16:30:52 -0300

    disableEsLint

--- app/index.tsx
+++ app/index.tsx
@@ -78,6 +78,7 @@ export default function App() {
 
             handleGithubOAuthCode(code)
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [response])
 
     if (!hasLoadedFonts) {
