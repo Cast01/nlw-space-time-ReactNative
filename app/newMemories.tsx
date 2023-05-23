e0571f9eebd64ec6cbb2f06e94d56b9bf81004b3
Cast01 <gabriel_floriano_castilho@outlook.com>
2023-05-22 21:16:09 -0300

    update: show selected image

--- app/newMemories.tsx
+++ app/newMemories.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react'
+
 import {
+    Image,
     ScrollView,
     Switch,
     Text,
@@ -9,14 +12,32 @@ import {
 
 import { Link } from 'expo-router'
 
+import * as ImagePicker from 'expo-image-picker'
+
 import Icon from '@expo/vector-icons/Feather'
 
 import NLWLogo from '../src/assets/images/nlw-space-logo.svg'
-import { useState } from 'react'
 
 export default function NewMemories() {
     const [isPublic, setIsPublic] = useState(false)
     const [content, setContent] = useState('')
+    const [preview, setPreview] = useState<string | null>(null)
+
+    async function openImagePicker() {
+        try {
+            // No permissions request is necessary for launching the image library
+            const result = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                quality: 1,
+            })
+
+            if (result.assets[0]) {
+                setPreview(result.assets[0].uri)
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    }
 
     async function submitNewMemory() {
         console.log(content, isPublic)
@@ -47,11 +68,22 @@ export default function NewMemories() {
                         Tornar memória púbica
                     </Text>
                 </View>
-                <TouchableOpacity className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20">
-                    <View className="flex-row">
-                        <Icon name="image" color={'#fff'} />
-                        <Text>Adicionar foto ou vídeo de capa.</Text>
-                    </View>
+                <TouchableOpacity
+                    className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20"
+                    onPress={openImagePicker}
+                >
+                    {preview ? (
+                        <Image
+                            source={{ uri: preview }}
+                            className="h-full w-full rounded-lg object-cover"
+                            alt=""
+                        />
+                    ) : (
+                        <View className="flex-row">
+                            <Icon name="image" color={'#fff'} />
+                            <Text>Adicionar foto ou vídeo de capa.</Text>
+                        </View>
+                    )}
                 </TouchableOpacity>
                 <ScrollView>
                     <TextInput
